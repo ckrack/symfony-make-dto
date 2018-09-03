e1c29cc90e1d878f3b015929b3a8b35052ae19f8
Clemens Krack <info@clemenskrack.com>
2018-09-03 22:32:58 +0200

    Add twig, webserver and framework-extra-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,7 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
